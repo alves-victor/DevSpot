366e0086b44b9253fa4610519b773674c5045ddb
Victor Alves <victorhugo_sp4@hotmail.com>
2022-04-24 18:35:28 -0300

    rota para dashboard

--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -4,8 +4,11 @@ const uploadConfig = require('./config/upload');
 const upload = multer(uploadConfig)
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 routes.post('/users', SessionController.store);
 routes.post('spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
