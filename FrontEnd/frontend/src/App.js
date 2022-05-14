e1bb33c0f9d35ac49019e65b5d387266f5ac2707
Victor Alves <victorhugo_sp4@hotmail.com>
2022-05-14 16:43:20 -0300

    realocação de html

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './assets/devspot.png';
+import Rotas from './routes';
 
 function App() {
   return (
@@ -7,16 +8,7 @@ function App() {
       <img className='logo' src={logo} alt='DevSpot Logo' />
 
       <div className='content'>
-        <p>
-          Ofereça <strong>Spots</strong> para programadores e encontre <strong>Talentos</strong> para sua empresa
-        </p>
-
-        <form>
-          <label htmlFor="email">EMAIL *</label>
-          <input type="email" id="email" />
-          <button className='btn' type='submit'>Entrar</button>
-        </form>
-
+        <Rotas />
       </div>
 
     </div>  
