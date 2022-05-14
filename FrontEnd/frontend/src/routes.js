4509d3d87db6cfa0c54c41ac9653615bf51261b7
Victor Alves <victorhugo_sp4@hotmail.com>
2022-05-14 16:48:42 -0300

    configuração das rotas

---
+++ FrontEnd/frontend/src/routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import NewSpot from './pages/NewSpot';
+
+export default function Rotas(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element = { <Login /> } />
+                <Route path="/dashboard" element = { <Dashboard /> } />
+                <Route path="/newspot" element = { <NewSpot />} /> 
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
