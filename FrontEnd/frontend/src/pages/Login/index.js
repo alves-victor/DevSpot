ae2547ec55ec216517b7b7e2a011160f05710141
Victor Alves <victorhugo_sp4@hotmail.com>
2022-05-14 16:47:59 -0300

    criação de páginas

---
+++ FrontEnd/frontend/src/pages/Login/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import api from '../../services/api';
+import { useState } from 'react';
+import { createBrowserHistory } from "history";
+
+export default function Login(){
+
+    const [email, setEmail] = useState('');
+
+    const history = createBrowserHistory();
+
+    async function handleSubmit(event){
+        event.preventDefault();
+
+        const response = await api.post('/sessions', { email });
+        const { _id } = response.data;
+        localStorage.setItem('user', _id);
+        
+        history.push('/dashboard');
+        history.go();
+    }
+
+    return(
+        <>
+            <p>
+                Ofereça <strong>Spots</strong> para programadores e encontre <strong>Talentos</strong> para sua empresa
+            </p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">EMAIL *</label>
+            <input type="email" id="email" 
+                value={email} 
+                onChange={event => setEmail(event.target.value)}
+            />
+            <button className='btn' type='submit'>Entrar</button>
+            </form>
+        </>
+    );
+}
